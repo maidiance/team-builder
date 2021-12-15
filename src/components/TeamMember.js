f6aab8e79345f15391a664250321b3a367287783
Winifred Lu <winnie.lu@comcast.net>
2021-12-15 00:37:40 -0500

    working on form and teamMember, some progress before running into eslint issues

---
+++ src/components/TeamMember.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function TeamMember(props) {
+    const { details } = props;
+    if (!details) {
+        return <h3>WOrking fetching your team member&apos;s details...</h3>
+    }
+
+    return (
+        <div className='teamMember container'>
+            <h2>{details.name}</h2>
+            <p>Email: {details.email}</p>
+            <p>Role: {details.role}</p>
+        </div>
+    )
+}
